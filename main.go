767bed8c3f50904c61c4a777b2b952fcd742ec82
NiseVoid <nisevoid@gmail.com>
2019-07-08 11:47:01 +0200

    Update for golangci-lint

--- main.go
+++ main.go
@@ -1,80 +1,76 @@
-package main
+package whitespace
 
 import (
-	"fmt"
 	"go/ast"
-	"go/parser"
 	"go/token"
-	"os"
 )
 
-func warn(file string, line int, msg string) {
-	fmt.Print(file, `:`, line, `:`, msg, "\n")
+// Message contains a message
+type Message struct {
+	Pos     token.Position
+	Message string
 }
 
-var fset *token.FileSet
-var comments []*ast.CommentGroup
+// Run runs this linter on the provided code
+func Run(file *ast.File, fset *token.FileSet) []Message {
+	var messages []Message
 
-func main() {
-	if len(os.Args) < 2 {
-		fmt.Println(`Usage:`, os.Args[0], ` target1 [target2]...`)
-		os.Exit(2)
-	}
-
-	for _, arg := range os.Args[1:] {
-		fset = token.NewFileSet()
-		pkgs, err := parser.ParseDir(fset, arg, nil, parser.ParseComments)
-		if err != nil {
-			panic(err)
+	for _, f := range file.Decls {
+		decl, ok := f.(*ast.FuncDecl)
+		if !ok {
+			continue
 		}
 
-		for _, pkg := range pkgs {
-			for _, file := range pkg.Files {
-				comments = file.Comments
-				for _, f := range file.Decls {
-					decl, ok := f.(*ast.FuncDecl)
-					if !ok {
-						continue
-					}
-					ast.Walk(&visitor{}, decl)
-				}
-			}
-		}
+		vis := visitor{file.Comments, fset, nil}
+		ast.Walk(&vis, decl)
+
+		messages = append(messages, vis.messages...)
 	}
+
+	return messages
 }
 
-type visitor struct{}
+type visitor struct {
+	comments []*ast.CommentGroup
+	fset     *token.FileSet
+	messages []Message
+}
 
-func (v visitor) Visit(node ast.Node) ast.Visitor {
+func (v *visitor) Visit(node ast.Node) ast.Visitor {
 	if node == nil {
 		return v
 	}
 
 	if stmt, ok := node.(*ast.BlockStmt); ok {
-		first, last := firstAndLast(stmt.Pos(), stmt.End(), stmt.List)
+		first, last := firstAndLast(v.comments, v.fset, stmt.Pos(), stmt.End(), stmt.List)
 
-		checkStart(stmt.Lbrace, first)
-		checkEnd(stmt.Rbrace, last)
+		if msg := checkStart(v.fset, stmt.Lbrace, first); msg != nil {
+			v.messages = append(v.messages, *msg)
+		}
+		if msg := checkEnd(v.fset, stmt.Rbrace, last); msg != nil {
+			v.messages = append(v.messages, *msg)
+		}
 	}
 
 	return v
 }
 
-func posLine(pos token.Pos) int {
+func posLine(fset *token.FileSet, pos token.Pos) int {
 	return fset.Position(pos).Line
 }
 
-func posFile(pos token.Pos) string {
-	return fset.Position(pos).Filename
-}
-
-func firstAndLast(start, end token.Pos, stmts []ast.Stmt) (ast.Node, ast.Node) {
+func firstAndLast(comments []*ast.CommentGroup, fset *token.FileSet, start, end token.Pos, stmts []ast.Stmt) (ast.Node, ast.Node) {
 	if len(stmts) == 0 {
 		return nil, nil
 	}
+
 	first, last := ast.Node(stmts[0]), ast.Node(stmts[len(stmts)-1])
 
 	for _, c := range comments {
+		if posLine(fset, c.Pos()) == posLine(fset, start) || posLine(fset, c.End()) == posLine(fset, end) {
+			continue
+		}
+
 		if c.Pos() < start || c.End() > end {
 			continue
 		}
@@ -89,22 +85,28 @@ func firstAndLast(start, end token.Pos, stmts []ast.Stmt) (ast.Node, ast.Node) {
 	return first, last
 }
 
-func checkStart(start token.Pos, first ast.Node) {
+func checkStart(fset *token.FileSet, start token.Pos, first ast.Node) *Message {
 	if first == nil {
-		return
+		return nil
 	}
 
-	if posLine(start)+1 < posLine(first.Pos()) {
-		warn(posFile(start), posLine(start)+1, `unnecessary newline`)
+	if posLine(fset, start)+1 < posLine(fset, first.Pos()) {
+		pos := fset.Position(start)
+		return &Message{pos, `unnecessary leading newline`}
 	}
+
+	return nil
 }
 
-func checkEnd(end token.Pos, last ast.Node) {
+func checkEnd(fset *token.FileSet, end token.Pos, last ast.Node) *Message {
 	if last == nil {
-		return
+		return nil
 	}
 
-	if posLine(end)-1 > posLine(last.End()) {
-		warn(posFile(end), posLine(end)-1, `unnecessary newline`)
+	if posLine(fset, end)-1 > posLine(fset, last.End()) {
+		pos := fset.Position(end)
+		return &Message{pos, `unnecessary trailing newline`}
 	}
+
+	return nil
 }
