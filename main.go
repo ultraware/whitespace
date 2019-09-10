6131e1ad2e02ff5feaa499c4217777711890231c
NiseVoid <nisevoid@gmail.com>
2019-09-10 10:23:15 +0200

    Don't crash on cgo

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func Run(file *ast.File, fset *token.FileSet) []Message {
 
 	for _, f := range file.Decls {
 		decl, ok := f.(*ast.FuncDecl)
-		if !ok {
+		if !ok || decl.Body == nil { // decl.Body can be nil for e.g. cgo
 			continue
 		}
 
